d59390011c6f2b179df92f7fbb57715e0d59350b
ragranato <robert.granato@gmail.com>
2023-07-03 19:00:25 -0400

    scaffolded categories, added sass styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-
+import './categories.styles.scss'
 
 
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Women's",
+    },
+    {
+      id: 5,
+      title: "Men's",
+    },
+  ];
+
   return (
-    <div className="App">
-      Hello World
+    <div className="categories-container">
+      {categories.map((category) => (
+        <div className="category-container" key={category.id}>
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
