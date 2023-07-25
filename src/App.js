3b0d319a83b357e0ea0d9cccc989859053eb91f7
ragranato <robert.granato@gmail.com>
2023-07-25 13:12:32 -0400

    25%

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
-
-// import Directory from './components/directory/directory.component';
-
+import Navigation from './routes/navigation/navigation.component';
 import Home from "./routes/home/home.component"
+
 
 
 const App = () => {
  
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
